985ce2a8658838071d32c32827a801ec039630c4
Francois Cocquemas <francois.cocquemas@gmail.com>
2020-12-14 13:56:50 -0500

    Fix count_by test

--- tests/testthat/test-sumstats.R
+++ tests/testthat/test-sumstats.R
@@ -3,8 +3,8 @@
 test_that("count_by returns counts", {
   require(data.table)
   DF = data.table(ID = c("b","b","b","a","a","c"), a = 1:6, b = 7:12, c = 13:18)
-  expect_equal(count_by(DF, "ID"), 3)
-  # dt <- data.table::as.data.table(iris)
-  # expect_equal(as.vector(count_by(dt, c("*", "Species", "Species-Petal.Width"))), c(150, 3, 27))
-  # expect_equal(as.vector(count_by(dt, c("*", "Species", "Species-Petal.Width"))), c(150, 3, 27))
+  expect_equal(count_by(DF, "ID"), c(ID=3))
+  expect_equal(count_by(DF, c("ID", "a")), c(ID=3, a=6))
+  DF2 <- data.table::as.data.table(iris)
+  expect_equal(as.vector(count_by(DF2, c("*", "Species", "Species-Petal.Width"))), c(150, 3, 27))
 })
